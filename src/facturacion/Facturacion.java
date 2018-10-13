b70f12701e2b6501ea7cc8f9973a53648db5f8a4
Aleexes <alexcrest@hotmail.com>
2018-10-13 16:40:11 -0600

    Iniciando el proyecto en cero desde netbeans

---
+++ src/facturacion/Facturacion.java
@@ -0,0 +1,16 @@
+package facturacion;
+
+/**
+ *
+ * @author alexc
+ */
+public class Facturacion {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
