eeec3d84e4f33a7fd96b08b466ac24697b471d79
Aleexes <alexcrest@hotmail.com>
2018-10-13 16:50:00 -0600

    Solo agreue unos print, probando el versionamiento de git (:

--- src/facturacion/Facturacion.java
+++ src/facturacion/Facturacion.java
@@ -10,7 +10,10 @@ public class Facturacion {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Probando cambios en git");
+        System.out.println("Probando cambios en git");
+        System.out.println("Probando cambios en git");
+        System.out.println("Probando cambios en git");
     }
     
 }
